82776595f3fb1f86eebadcc0e045bf345edd3071
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-08-11 16:28:19 -0700

    Show matching pixel percentage

--- cameraTester.py
+++ cameraTester.py
@@ -9,10 +9,21 @@ cam = cv.VideoCapture(0)
 cv.namedWindow('Frame')
 cv.namedWindow('Extra frame')
 
-print(cameraTesterConfig.roiColorFilters)
+def getPercentage(msk):
+	# Count the number of matching pixels
+	matching_pixels = cv.countNonZero(msk)
+
+	# Calculate the total number of pixels
+	total_pixels = msk.size
+
+	# Calculate the percentage of matching pixels
+	percentage_matching = (matching_pixels / total_pixels) * 100
+
+	return percentage_matching
+
 
 if not cam.isOpened():
-	print("Error: Unable to access the webcam.")
+	print("Error: Unable to access the cam.")
 else:
 	while True:
 		# Capture frame-by-frame from the cam
@@ -26,9 +37,13 @@ else:
 				img_crop = frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
 				hsv = cv.cvtColor(img_crop, cv.COLOR_BGR2HSV)
 				msk = cv.inRange(hsv, np.array(color_data[0]), np.array(color_data[1]))
+				
+				percentage_matching = getPercentage(msk)
+				percentage_color = (0, 255, 0) if percentage_matching > 80 else (0, 0, 255)
 				filtered = cv.bitwise_and(img_crop, img_crop, mask= msk)
 
 				frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])] = filtered
+				cv.putText(frame, f'{int(percentage_matching)}%', (int(roi[0]+roi[3]/5), int(roi[1]+roi[2]/1.6)) ,cv.FONT_HERSHEY_SIMPLEX, 0.4, percentage_color, 1)
 
 			cv.imshow('Frame', frame)
 			cv.imshow('Extra frame', filtered)
