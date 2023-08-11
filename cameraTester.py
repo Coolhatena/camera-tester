d47edaca21f57e87734bf8a1aa35eced429472e1
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-08-11 15:05:24 -0700

    Add roi modularity

--- cameraTester.py
+++ cameraTester.py
@@ -1,11 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+import cameraTesterConfig
+
 # Open the cam (use 0 for the default camera)
 cam = cv.VideoCapture(0)
 
 cv.namedWindow('Frame')
 cv.namedWindow('Extra frame')
+
+print(cameraTesterConfig.roiColorFilters)
 
 if not cam.isOpened():
 	print("Error: Unable to access the webcam.")
@@ -14,15 +18,18 @@ else:
 		# Capture frame-by-frame from the cam
 		ret, frame = cam.read()
 
-		roi = [160, 120, 80, 80]
-		img_crop = frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+		roi_color_filters = cameraTesterConfig.roiColorFilters
 
 		if ret:
-			hsv = cv.cvtColor(img_crop, cv.COLOR_BGR2HSV)
-			msk = cv.inRange(hsv, np.array([0, 25, 107]), np.array([25, 255, 255]))
-			filtered = cv.bitwise_and(img_crop, img_crop, mask= msk)
+			for color_data in roi_color_filters: 
+				roi = color_data[2]
+				img_crop = frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+				hsv = cv.cvtColor(img_crop, cv.COLOR_BGR2HSV)
+				msk = cv.inRange(hsv, np.array(color_data[0]), np.array(color_data[1]))
+				filtered = cv.bitwise_and(img_crop, img_crop, mask= msk)
 
-			frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])] = filtered
+				frame[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])] = filtered
+
 			cv.imshow('Frame', frame)
 			cv.imshow('Extra frame', filtered)
 
